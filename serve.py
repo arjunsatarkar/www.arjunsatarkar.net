3e98f1bf14058a631622a149a108de53d0a9f551
Arjun Satarkar <me@arjunsatarkar.net>
2023-06-03 03:58:16 +0530

    Use args rather than hard-coding in serve.py

--- serve.py
+++ serve.py
@@ -1,24 +1,36 @@
 #!/usr/bin/env python3
-import gevent.os, gevent.subprocess
 import gevent.monkey
 
 gevent.monkey.patch_all()
 
+import argparse
 import pathlib
 import bottle
 
-DEBUG = False
-HOST = "127.0.0.1"
-PORT = 9001
+
+def parse_port(port_str: str):
+    port = int(port_str)
+    if port <= 0:
+        raise ValueError(f"port must be positive")
+    return port
+
+parser = argparse.ArgumentParser(prog="www.arjunsatarkar.net server")
+parser.add_argument("--host", default="127.0.0.1")
+parser.add_argument("--port", default=9001, type=parse_port)
+args = parser.parse_args()
+
 SITE_ROOT = pathlib.Path("sitebuild")
+
 
 @bottle.route("/")
 def index():
     return bottle.static_file("index.html", SITE_ROOT / "html")
 
+
 @bottle.route("/static/<static_file_path:path>")
 def static(static_file_path):
     return bottle.static_file(static_file_path, SITE_ROOT / "static")
+
 
 @bottle.error(404)
 def error404(error):
@@ -26,5 +38,6 @@ def error404(error):
     response.status = 404
     return response
 
+
 if __name__ == "__main__":
-    bottle.run(host=HOST, port=PORT, server="gevent", debug=DEBUG)
+    bottle.run(host=args.host, port=args.port, server="gevent", debug=False)
