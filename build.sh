5d4a8f577eeb9e6de0cc888377a636352af3069a
Arjun Satarkar <me@arjunsatarkar.net>
2023-06-02 22:38:10 +0000

    build.sh: create sitebuild/ if not present

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+mkdir -p sitebuild
 cp -r sitesrc/static sitebuild/static
 cp sitesrc/404.html sitebuild/
 asciidoctor -a webfonts! -a last-update-label! -a stylesheet=main.css -a stylesdir=/static/styles -a linkcss -a copycss! -a favicon=/static/media/favicon.svg --destination-dir=sitebuild/html sitesrc/asciidoc/*.adoc
