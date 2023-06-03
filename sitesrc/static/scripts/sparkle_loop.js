013087cea05e67d5ab689a01d4a2e7da61bba548
Arjun Satarkar <me@arjunsatarkar.net>
2023-06-03 06:02:21 +0530

    Allow sparkle loop element to be focused, handle enter key on it

--- sitesrc/static/scripts/sparkle_loop.js
+++ sitesrc/static/scripts/sparkle_loop.js
@@ -5,5 +5,11 @@
         "💫": "🌟",
     };
     const sparkle_loop_elem = document.querySelector("#sparkle_loop");
+    sparkle_loop_elem.setAttribute("tabindex", "0");
     sparkle_loop_elem.addEventListener("click", (_) => sparkle_loop_elem.innerText = sparkle_loop[sparkle_loop_elem.innerText]);
+    sparkle_loop_elem.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+            sparkle_loop_elem.innerText = sparkle_loop[sparkle_loop_elem.innerText]
+        }
+    });
 })();
