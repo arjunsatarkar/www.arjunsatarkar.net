5c064238f7834895b9a497e8560d324cd9de889a
Arjun Satarkar <me@arjunsatarkar.net>
2023-06-03 06:16:02 +0530

    Reduce code duplication in sparkle_loop.js

--- sitesrc/static/scripts/sparkle_loop.js
+++ sitesrc/static/scripts/sparkle_loop.js
@@ -6,10 +6,13 @@
     };
     const sparkle_loop_elem = document.querySelector("#sparkle_loop");
     sparkle_loop_elem.setAttribute("tabindex", "0");
-    sparkle_loop_elem.addEventListener("click", (_) => sparkle_loop_elem.innerText = sparkle_loop[sparkle_loop_elem.innerText]);
+    function update_sparkle_loop() {
+        sparkle_loop_elem.innerText = sparkle_loop[sparkle_loop_elem.innerText];
+    }
+    sparkle_loop_elem.addEventListener("click", (_) => update_sparkle_loop());
     sparkle_loop_elem.addEventListener("keydown", (e) => {
         if (e.key === "Enter") {
-            sparkle_loop_elem.innerText = sparkle_loop[sparkle_loop_elem.innerText]
+            update_sparkle_loop();
         }
     });
 })();
