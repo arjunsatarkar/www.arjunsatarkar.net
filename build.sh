8db84c83276ab49ce68539f4339da23627904a9a
Arjun Satarkar <me@arjunsatarkar.net>
2023-06-10 05:21:56 +0530

    Add minification, README, ...
    Also added a license to sitesrc/static/scripts/sparkle_loop.js (only
    that file).
    
    The minification has dependencies but is optional; build.sh will
    proceed with a straight copy if it fails (eg. if the dependencies are
    not installed).

--- build.sh
+++ build.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+BASEDIR="$PWD"
+
 mkdir -p sitebuild
-cp -r sitesrc/static sitebuild/
+
 asciidoctor -a webfonts! -a last-update-label! -a stylesheet=main.css -a stylesdir=/static/styles -a linkcss -a copycss! -a favicon=/static/media/favicon.svg -a docinfodir=docinfo --destination-dir=sitebuild/html sitesrc/asciidoc/*.adoc
+
+basic_build_static () {
+    cp -r sitesrc/static sitebuild/
+}
+
+basic_build_static
+
+{
+    cd sitesrc/static/scripts/ &&
+    for f in *.js; do
+        npx terser "$f" > "$BASEDIR/sitebuild/static/scripts/$f"
+    done &&
+    cd "$BASEDIR/sitesrc/static/styles/" &&
+    for f in *.css; do
+        npx postcss "$f" > "$BASEDIR/sitebuild/static/styles/$f"
+    done &&
+    cd "$BASEDIR"
+} || {
+    >&2 echo "WARN: minify step failed; proceeding without minification."
+    cd "$BASEDIR" &&
+    basic_build_static
+}
