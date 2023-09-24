bdcbac4ed9942e45926cd4ce8f70baf0b36bb2da
Arjun Satarkar <me@arjunsatarkar.net>
2023-09-23 23:44:58 -0400

    Fresh start: make home page

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-mkdir -p sitebuild
-
-asciidoctor -a webfonts! -a last-update-label! -a stylesheet=main.css -a stylesdir=/static/styles -a linkcss -a copycss! -a favicon=/static/media/favicon.svg -a docinfodir=docinfo --destination-dir=sitebuild/ sitesrc/*.adoc
-
-cp -r sitesrc/static sitebuild/
